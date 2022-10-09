91297e77e17db019172fe6c1fd12486353e87579
federerkristijan <federer.kristijan@gmail.com>
2022-10-09 15:26:30 +0200

    arrays and types

--- basics.ts
+++ basics.ts
@@ -8,3 +8,22 @@ let userName: string;
 let isReal: boolean;
 
 isReal = true;
+
+// complex types
+
+// array of strings
+let hobbies: string[];
+
+hobbies = ['sleeping', 'eating', 'coding'];
+
+// defining type as an object
+let person: {
+  name: string,
+  age: number
+};
+
+// defining an array of objects
+let people: {
+  name: string,
+  age: number
+}[];
