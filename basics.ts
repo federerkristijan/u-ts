935da7c7d0953a1a288ac910bd3d7b9648091665
federerkristijan <federer.kristijan@gmail.com>
2022-10-09 14:16:28 +0200

    base types

---
+++ basics.ts
@@ -0,0 +1,10 @@
+// Primitives
+// ':' type assignment && '=' value assignment
+let age: number = 42;
+
+// capital String points at JS Object, small string points at type
+let userName: string;
+
+let isReal: boolean;
+
+isReal = true;
